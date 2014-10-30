33f6572bbdb1340470344c7dc57107776a990ddf
Roman Miroshnychenko <romanvm@yandex.ru>
2014-10-30 21:59:46 +0200

    Code refactoring Romoved local pyxbmct module

--- script.pyxbmct.demo/default.py
+++ script.pyxbmct.demo/default.py
@@ -4,14 +4,15 @@
 # and usage of PyXBMCt framework.
 
 import os
-import xbmc, xbmcaddon, xbmcgui
-from pyxbmct.addonwindow import *
+import xbmc
+import xbmcaddon
+import pyxbmct.addonwindow as pyxbmct
 
 _addon = xbmcaddon.Addon()
 _addon_path = _addon.getAddonInfo('path')
 
 
-class MyAddon(AddonDialogWindow):
+class MyAddon(pyxbmct.AddonDialogWindow):
 
     def __init__(self, title=''):
         super(MyAddon, self).__init__(title)
@@ -20,95 +21,102 @@ class MyAddon(AddonDialogWindow):
         self.set_active_controls()
         self.set_navigation()
         # Connect a key action (Backspace) to close the window.
-        self.connect(ACTION_NAV_BACK, self.close)
+        self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
 
     def set_info_controls(self):
         # Demo for PyXBMCt UI controls.
-        no_int_label = Label('Information output', alignment=ALIGN_CENTER)
+        no_int_label = pyxbmct.Label('Information output', alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(no_int_label, 0, 0, 1, 2)
         #
-        label_label = Label('Label')
+        label_label = pyxbmct.Label('Label')
         self.placeControl(label_label, 1, 0)
         # Label
-        self.label = Label('Simple label')
+        self.label = pyxbmct.Label('Simple label')
         self.placeControl(self.label, 1, 1)
         #
-        fadelabel_label = Label('FadeLabel')
+        fadelabel_label = pyxbmct.Label('FadeLabel')
         self.placeControl(fadelabel_label, 2, 0)
         # FadeLabel
-        self.fade_label = FadeLabel()
+        self.fade_label = pyxbmct.FadeLabel()
         self.placeControl(self.fade_label, 2, 1)
         self.fade_label.addLabel('Very long string can be here.')
         #
-        textbox_label = Label('TextBox')
+        textbox_label = pyxbmct.Label('TextBox')
         self.placeControl(textbox_label, 3, 0)
         # TextBox
-        self.textbox = TextBox()
+        self.textbox = pyxbmct.TextBox()
         self.placeControl(self.textbox, 3, 1, 2, 1)
         self.textbox.setText('Text box.\nIt can contain several lines.')
         #
-        image_label = Label('Image')
+        image_label = pyxbmct.Label('Image')
         self.placeControl(image_label, 5, 0)
         # Image
-        self.image = Image(os.path.join(_addon_path, 'xbmc-logo.png'))
+        self.image = pyxbmct.Image(os.path.join(_addon_path, 'xbmc-logo.png'))
         self.placeControl(self.image, 5, 1, 2, 1)
 
     def set_active_controls(self):
-        int_label = Label('Interactive Controls', alignment=ALIGN_CENTER)
+        int_label = pyxbmct.Label('Interactive Controls', alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(int_label, 0, 2, 1, 2)
         #
-        radiobutton_label = Label('RadioButton')
+        radiobutton_label = pyxbmct.Label('RadioButton')
         self.placeControl(radiobutton_label, 1, 2)
         # RadioButton
-        self.radiobutton = RadioButton('Off')
+        self.radiobutton = pyxbmct.RadioButton('Off')
         self.placeControl(self.radiobutton, 1, 3)
         self.connect(self.radiobutton, self.radio_update)
         #
-        edit_label = Label('Edit')
+        edit_label = pyxbmct.Label('Edit')
         self.placeControl(edit_label, 2, 2)
         # Edit
-        self.edit = Edit('Edit')
+        self.edit = pyxbmct.Edit('Edit')
         self.placeControl(self.edit, 2, 3)
         # Additional properties must be changed after (!) displaying a control.
         self.edit.setText('Enter text here')
         #
-        list_label = Label('List')
+        list_label = pyxbmct.Label('List')
         self.placeControl(list_label, 3, 2)
         #
-        self.list_item_label = Label('', textColor='0xFF808080')
+        self.list_item_label = pyxbmct.Label('', textColor='0xFF808080')
         self.placeControl(self.list_item_label, 4, 2)
         # List
-        self.list = List()
+        self.list = pyxbmct.List()
         self.placeControl(self.list, 3, 3, 3, 1)
         # Add items to the list
-        items = ['Item %s' % i for i in range(1, 8)]
+        items = ['Item {0}'.format(i) for i in range(1, 8)]
         self.list.addItems(items)
         # Connect the list to a function to display which list item is selected.
-        self.connect(self.list, lambda: xbmc.executebuiltin('Notification(Note!,%s selected.)' %
-                                            self.list.getListItem(self.list.getSelectedPosition()).getLabel()))
+        self.connect(self.list, lambda: xbmc.executebuiltin('Notification(Note!,{0} selected.)'.format(
+            self.list.getListItem(self.list.getSelectedPosition()).getLabel())))
         # Connect key and mouse events for list navigation feedback.
         self.connectEventList(
-            [ACTION_MOVE_DOWN, ACTION_MOVE_UP, ACTION_MOUSE_WHEEL_DOWN, ACTION_MOUSE_WHEEL_UP, ACTION_MOUSE_MOVE],
+            [pyxbmct.ACTION_MOVE_DOWN,
+             pyxbmct.ACTION_MOVE_UP,
+             pyxbmct.ACTION_MOUSE_WHEEL_DOWN,
+             pyxbmct.ACTION_MOUSE_WHEEL_UP,
+             pyxbmct.ACTION_MOUSE_MOVE],
             self.list_update)
         # Slider value label
         SLIDER_INIT_VALUE = 25.0
-        self.slider_value = Label(str(SLIDER_INIT_VALUE), alignment=ALIGN_CENTER)
+        self.slider_value = pyxbmct.Label(str(SLIDER_INIT_VALUE), alignment=pyxbmct.ALIGN_CENTER)
         self.placeControl(self.slider_value, 6, 3)
         #
-        slider_caption = Label('Slider')
+        slider_caption = pyxbmct.Label('Slider')
         self.placeControl(slider_caption, 7, 2)
         # Slider
-        self.slider = Slider()
+        self.slider = pyxbmct.Slider()
         self.placeControl(self.slider, 7, 3, pad_y=10)
         self.slider.setPercent(SLIDER_INIT_VALUE)
         # Connect key and mouse events for slider update feedback.
-        self.connectEventList([ACTION_MOVE_LEFT, ACTION_MOVE_RIGHT, ACTION_MOUSE_DRAG, ACTION_MOUSE_LEFT_CLICK],
-            self.slider_update)
+        self.connectEventList([pyxbmct.ACTION_MOVE_LEFT,
+                               pyxbmct.ACTION_MOVE_RIGHT,
+                               pyxbmct.ACTION_MOUSE_DRAG,
+                               pyxbmct.ACTION_MOUSE_LEFT_CLICK],
+                              self.slider_update)
         #
-        button_label = Label('Button')
+        button_label = pyxbmct.Label('Button')
         self.placeControl(button_label, 8, 2)
         # Button
-        self.button = Button('Close')
+        self.button = pyxbmct.Button('Close')
         self.placeControl(self.button, 8, 3)
         # Connect control to close the window.
         self.connect(self.button, self.close)
@@ -132,7 +140,7 @@ class MyAddon(AddonDialogWindow):
         # Update slider value label when the slider nib moves
         try:
             if self.getFocus() == self.slider:
-                self.slider_value.setLabel('%.1f' % self.slider.getPercent())
+                self.slider_value.setLabel('{:.1F}'.format(self.slider.getPercent()))
         except (RuntimeError, SystemError):
             pass
 
@@ -159,10 +167,9 @@ class MyAddon(AddonDialogWindow):
                                 ('WindowClose', 'effect=fade start=100 end=0 time=500',)])
 
 
-
-def main():
+if __name__ == '__main__':
     window = MyAddon('PyXBMCt Demo')
     window.doModal()
-
-if __name__ == '__main__':
-    main()
+    # Destroy the instance explicitly because
+    # underlying xbmcgui classes are not garbage-collected on exit.
+    del window
