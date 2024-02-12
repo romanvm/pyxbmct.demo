f0647043cfc7358bd8a63fb42bdbebcaabe5ecce
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-02-12 11:32:53 +0200

    Remove a label for Edit control example

--- script.pyxbmct.demo/default.py
+++ script.pyxbmct.demo/default.py
@@ -74,7 +74,7 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         edit_label = pyxbmct.Label('Edit')
         self.placeControl(edit_label, 2, 2)
         # Edit
-        self.edit = pyxbmct.Edit('Edit')
+        self.edit = pyxbmct.Edit()
         self.placeControl(self.edit, 2, 3)
         # Additional properties must be changed after (!) displaying a control.
         self.edit.setText('Enter text here')
